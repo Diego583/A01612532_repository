69b5b3f3eb14026fd2ee0427cfe73330e51c8765
A01612532 <A01612532@itesm.mx>
2021-03-01 00:27:42 -0600

    12

--- lab12/app.js
+++ lab12/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-router.get('/git', (request, response, next) => {
-    response.sendFile(path.join(__dirname, '..', 'views', 'el-archivo.html'));
+app.get('/git', (request, response, next) => {
+    response.sendFile(path.join(__dirname, 'views', 'git.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response, next) => {
+	let html = '<h1>RUTAS</h1>';
+    html += '<form action="/git" method="GET"><button>TIENDA</button></form>'; 
+    response.send(html); 
 });
 
 app.use((request, response, next) => {
